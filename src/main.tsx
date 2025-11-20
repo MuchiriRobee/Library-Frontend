658cbbf96240fb38721f14e3470214ac9c36797b
MuchiriRobee <robeemuchiri@gmail.com>
2025-11-20 14:34:53 +0300

    added vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { AuthProvider } from "@/context/AuthContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "sonner";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,6 +22,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <App />
         <Toaster richColors position="top-center" />
         <ReactQueryDevtools initialIsOpen={false} />
+        <SpeedInsights />
       </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
